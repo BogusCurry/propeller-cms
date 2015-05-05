0487f58e6f93578a25bcbf01ea50df544e67f779
pbelanger <patrick.belanger@gmail.com>
2015-05-05 10:52:54 -0400

    Initial commit - PageDao
    Initial commit - PageDao

---
+++ Apps/DawnFramework/Cms/Lib/PagesDao.class.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Propeller
+ * CMS (Content Management System)
+ * Using Dawn MVC PHP Framework (5.4+)
+ *
+ * @description
+ * @file            PagesDao.class.php
+ * @version         1
+ * @package         Lib
+ * @author          Patrick Bélanger (patrick.belanger <dot> gmail <dot> com)
+ * @licence         Apache 2.0  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Copyright © 2015, Patrick Bélanger and contributors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apps\DawnFramework\Cms\Lib;
+
+interface PagesDao {
+
+    public function getPageId($id);
+
+    public function getPageName($name = 'home');
+
+}
